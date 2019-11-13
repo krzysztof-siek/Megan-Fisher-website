85d04516391c6086177a3a3974a334b673386062
krzysztof-siek <krzysztof.siek01@gmail.com>
2019-11-13 17:01:58 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const btn = document.querySelector(".hamburger-menu")
+const toggle = document.querySelectorAll(".toggle")
+// const nav = document.querySelector('.nav-list')
+
+
+
+btn.addEventListener("click", () => {
+    for (let i = 0; i < toggle.length; i++) {
+        toggle[i].classList.toggle("open");
+    }
+    // nav.classList.toggle('open')
+})
