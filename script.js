c1c1d4ff4d8e939e1a5c76e0149dc005d313a9d3
krzysztof-siek <krzysztof.siek01@gmail.com>
2019-12-19 12:07:52 +0100

    reset - start from begining

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-const btn = document.querySelector(".hamburger-menu")
-const toggle = document.querySelectorAll(".toggle")
-const nav = document.querySelector('.nav')
 
-
-
-btn.addEventListener("click", () => {
-    for (let i = 0; i < toggle.length; i++) {
-        toggle[i].classList.toggle("open");
-    }
-    nav.classList.toggle('open')
-})
