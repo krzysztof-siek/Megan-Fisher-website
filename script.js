39c2ac6548406543c50be32ee55c87c2af343877
krzysztof-siek <krzysztof.siek01@gmail.com>
2019-12-19 20:21:23 +0100

    add responsive to header. Add nav-button change and some animation

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+window.addEventListener('DOMContentLoaded', (event) => {
 
+    window.addEventListener('resize', () => {
+        let windowWidth = window.innerWidth;
+        if (windowWidth > 768) {
+            const navLinks = document.querySelector('.nav-links');
+            const navButton = document.querySelector('.nav-button');
+            navLinks.classList.remove('nav-open')
+            navButton.classList.remove("isOpen")
+        }
+
+    })
+
+    const navButton = document.querySelector('.nav-button');
+    navButton.addEventListener('click', () => {
+        const navLinks = document.querySelector('.nav-links');
+        navButton.classList.toggle("isOpen")
+        navLinks.classList.toggle('nav-open')
+    })
+
+
+
+
+
+
+
+
+});
